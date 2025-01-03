51b6cdfe214399f3d610ff3c7c5807cb0a66bcee
eran.zaksh <eranzaksh@gmail.com>
2025-01-03 21:44:44 +0200

    Trying to automate iso install

---
+++ iso_install/create_autoinstaller_iso.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install 7zip wget xorriso whois
+
+mkdir ubuntu-iso-sources
+
+7z -y x ubuntu-22.04.1-live-server-amd64.iso -oubuntu-iso-sources
+
+cd ubuntu-iso-sources
+
+mv \[BOOT\] ../BOOT
+GRUB_CFG="ubuntu-iso-sources/boot/grub/grub.cfg"
+MENU_ENTRY='menuentry "Autoinstall Ubuntu Server" {
+    set gfxpayload=keep
+    linux   /casper/vmlinuz quiet autoinstall ds=nocloud\\;s=/cdrom/server/  ---
+    initrd  /casper/initrd
+}'
+sed -i "/set menu_color_highlight=black\/light-gray/a $MENU_ENTRY" "$GRUB_CFG"
+echo "Menu entry added successfully."
+
+# mkdir -p ubuntu-iso-sources/server
+# mv user-data ubunt ubuntu-iso-sources/server/
+
+# xorriso -as mkisofs -r \
+#   -V 'Ubuntu 22.04 LTS (Auto Install)' \
+#   -o ../ubuntu-22.04-autoinstall.iso \
+#   --grub2-mbr ../BOOT/1-Boot-NoEmul.img \
+#   -partition_offset 16 \
+#   --mbr-force-bootable \
+#   -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b ../BOOT/2-Boot-NoEmul.img \
+#   -appended_part_as_gpt \
+#   -iso_mbr_part_type a2a0d0ebe5b9334487c068b6b72699c7 \
+#   -c '/boot.catalog' \
+#   -b '/boot/grub/i386-pc/eltorito.img' \
+#   -no-emul-boot -boot-load-size 4 -boot-info-table --grub2-boot-info \
+#   -eltorito-alt-boot \
+#   -e '--interval:appended_partition_2:::' \
+#   -no-emul-boot .
+
+# cd ..
+
+# qemu-img create -f raw ubuntu-disk.img 10G
+
+# qemu-system-x86_64 -m 4096 -smp 4 \
+#   -drive file=ubuntu-disk.img,format=raw \
+#   -cdrom ./ubuntu-22.04-autoinstall.iso \
+#   -boot d 
+
+
+
+
+
+
+# VBoxManage createvm --name "UbuntuServer" --register --ostype "Ubuntu_64"
+# VBoxManage modifyvm "UbuntuServer" --memory 2048 --cpus 2
+
+# # Create a virtual hard disk in VDI format
+# VBoxManage createhd --filename ~/VirtualBox\ VMs/UbuntuServer/ubuntu-disk.vdi --size 10240 --format VDI
+
+# # Create a SATA controller if it doesn't exist (recommended)
+# VBoxManage storagectl "UbuntuServer" --name "SATA Controller" --add sata --controller IntelAhci
+
+# # Attach the hard disk to the SATA controller
+# VBoxManage storageattach "UbuntuServer" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium ~/VirtualBox\ VMs/UbuntuServer/ubuntu-disk.vdi
+
+# # Attach the autoinstall ISO to the VM's CD/DVD drive (using SATA)
+# VBoxManage storageattach "UbuntuServer" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium ./ubuntu-22.04-autoinstall.iso
+# VBoxManage modifyvm "UbuntuServer" --boot1 dvd --boot2 disk
+
+# VBoxManage startvm "UbuntuServer" --type headless
+
+# VBoxManage export UbuntuServer -o ubuntu-2204.ova
