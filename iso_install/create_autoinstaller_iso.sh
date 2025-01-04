fae5c2ebc0c39af3124264885cced43d544a3081
eran.zaksh <eranzaksh@gmail.com>
2025-01-04 12:49:12 +0200

    Script is working. complete automation for installing ubuntu os on vm and exporting ova

--- iso_install/create_autoinstaller_iso.sh
+++ iso_install/create_autoinstaller_iso.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# sudo apt update && sudo apt install 7zip wget xorriso whois
+sudo apt update && sudo apt install -y 7zip wget xorriso whois
 
-# mkdir ubuntu-iso-sources
+mkdir ubuntu-iso-sources
 
-# 7z -y x ubuntu-22.04.1-live-server-amd64.iso -oubuntu-iso-sources
+7z -y x ubuntu-22.04.1-live-server-amd64.iso -oubuntu-iso-sources
 
-# cd ubuntu-iso-sources
+cd ubuntu-iso-sources
 
-# mv \[BOOT\] ../BOOT
-# cd ..
+mv \[BOOT\] ../BOOT
+cd ..
 GRUB_CFG="./ubuntu-iso-sources/boot/grub/grub.cfg"
 MENU_ENTRY='menuentry "Autoinstall Ubuntu Server" {
     set gfxpayload=keep
@@ -24,12 +24,14 @@ else
     echo "Menu entry already exists."
 fi
 
+echo "moving user-data to ubuntu-iso-sources/server/"
+
 mkdir -p ubuntu-iso-sources/server
 cp user-data ubuntu-iso-sources/server/
 touch ubuntu-iso-sources/server/meta-data
 
 cd ubuntu-iso-sources
-
+echo "Creating an autoinstaller iso..."
 xorriso -as mkisofs -r \
   -V 'Ubuntu 22.04 LTS (Auto Install)' \
   -o ../ubuntu-22.04-autoinstall.iso \
@@ -45,22 +47,12 @@ xorriso -as mkisofs -r \
   -eltorito-alt-boot \
   -e '--interval:appended_partition_2:::' \
   -no-emul-boot .
-
+echo "Autoinstaller complete"
 cd ..
 
-# qemu-img create -f raw ubuntu-disk.img 10G
-# sleep 1
 
-# qemu-system-x86_64 -m 4096 -smp 4 \
-#   -drive file=ubuntu-disk.img,format=raw \
-#   -cdrom ./ubuntu-22.04-autoinstall.iso \
-#   -boot d 
-
-# echo "Hello"
-
-
-
-
+echo "Creating VM and installing the ubuntu os..."
+sleep 1
 VBoxManage createvm --name "UbuntuServer" --register --ostype "Ubuntu_64"
 VBoxManage modifyvm "UbuntuServer" --memory 4096 --cpus 4
 
@@ -76,8 +68,23 @@ VBoxManage storageattach "UbuntuServer" --storagectl "SATA Controller" --port 0 
 # Attach the autoinstall ISO to the VM's CD/DVD drive (using SATA)
 VBoxManage storageattach "UbuntuServer" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium ./ubuntu-22.04-autoinstall.iso
 VBoxManage modifyvm "UbuntuServer" --boot1 dvd
-#  --boot2 disk
-VBoxManage startvm "UbuntuServer" 
-# --type headless
 
-# VBoxManage export UbuntuServer -o ubuntu-2204.ova
+VBoxManage startvm "UbuntuServer" --type headless
+
+VM_NAME="UbuntuServer"
+
+is_powered_off() {
+    status=$(VBoxManage showvminfo "$VM_NAME" --machinereadable | grep ^VMState=)
+    [[ "$status" == *"poweroff"* ]]
+}
+
+while ! is_powered_off; do
+    echo "Waiting for VM to finish installation and $VM_NAME to power off..."
+    sleep 30
+done
+
+echo "VM $VM_NAME has powered off"
+sleep 1
+echo "Exporting ova file..."
+VBoxManage export UbuntuServer -o ubuntu-2204.ova
+echo "ova file exported successfully!"
